53ef630f9ca984d4ee018e31f26407333a204f4f
Ivan Boiun <waynejey@yandex.ru>
2025-02-14 18:56:35 +0300

    workflow 13

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -123,10 +123,10 @@ FILE_UPLOAD_HANDLERS = [
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 ]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'http://127.0.0.1:9000',
-#     'http://localhost:9000',
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
